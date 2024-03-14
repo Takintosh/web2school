4ce56c5938929aec82af60da4ce234a913a654e0
Amilcar Santana <takintosh@gmail.com>
2024-03-14 14:07:36 -0300

    First Commit

---
+++ src/main/java/com/takintosh/web2school/Web2schoolApplication.java
@@ -0,0 +1,13 @@
+package com.takintosh.web2school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web2schoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Web2schoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/takintosh/web2school/Web2schoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.takintosh.web2school;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web2schoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
