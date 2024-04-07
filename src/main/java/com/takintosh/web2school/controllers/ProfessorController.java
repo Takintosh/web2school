66fcc12a8b4eda2ebd2ac0fd988d40dc7750869e
Amilcar Santana <takintosh@gmail.com>
2024-04-07 17:38:53 -0300

    professor CRUD updated

--- src/main/java/com/takintosh/web2school/controllers/ProfessorController.java
+++ src/main/java/com/takintosh/web2school/controllers/ProfessorController.java
@@ -1,6 +1,7 @@
 package com.takintosh.web2school.controllers;
 
 import com.takintosh.web2school.dtos.ProfessorRecordDto;
+import com.takintosh.web2school.dtos.StudentRecordDto;
 import com.takintosh.web2school.models.CourseModel;
 import com.takintosh.web2school.models.ProfessorModel;
 import com.takintosh.web2school.models.StudentModel;
@@ -11,7 +12,10 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 import java.util.Optional;
@@ -20,7 +24,8 @@ import java.util.UUID;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
-@RestController
+@Controller
+@RequestMapping("/professors")
 public class ProfessorController {
 
     @Autowired
@@ -28,87 +33,117 @@ public class ProfessorController {
     @Autowired
     CourseRepository courseRepository;
 
+    // Create a new student form
+    @GetMapping("/create")
+    public ModelAndView professorCreateGet() {
+        ModelAndView mv = new ModelAndView("professors/Create");
+        List<CourseModel> courses = courseRepository.findAll();
+        mv.addObject("courses", courses);
+        return mv;
+    }
+
     // Create a new professor
-    @PostMapping("/professors")
-    public ResponseEntity<ProfessorModel> saveProfessor(@RequestBody @Valid ProfessorRecordDto professorRecordDto) {
-        var professorModel = new ProfessorModel();
+    @PostMapping("/create")
+    public String professorCreatePost(@Valid @ModelAttribute ProfessorRecordDto professorRecordDto, BindingResult result) {
+        if (result.hasErrors()) {
+            return "professors/Create";
+        }
+
+        ProfessorModel professorModel = new ProfessorModel();
         BeanUtils.copyProperties(professorRecordDto, professorModel);
-        return ResponseEntity.status(HttpStatus.CREATED).body(professorRepository.save(professorModel));
+
+        CourseModel course = courseRepository.findById(professorRecordDto.courseId()).get();
+        professorModel.setCourse(course);
+
+        professorRepository.save(professorModel);
+        return "redirect:/professors/";
     }
 
     // Get all professors
-    @GetMapping("/professors")
-    public ResponseEntity<List<ProfessorModel>> getAllProfessors() {
-        List<ProfessorModel> professorsList = professorRepository.findAll();
-        if(!professorsList.isEmpty()) {
-            for(ProfessorModel professor : professorsList) {
-                UUID id = professor.getIdPerson();
-                professor.add(linkTo(methodOn(ProfessorController.class).getOneProfessor(id)).withSelfRel());
-            }
-        }
-        return ResponseEntity.status(HttpStatus.OK).body(professorsList);
+    @GetMapping("/")
+    public ModelAndView professorsList() {
+        ModelAndView mv = new ModelAndView("professors/ReadAll");
+        List<ProfessorModel> professors = professorRepository.findAll();
+        mv.addObject("professors", professors);
+        return mv;
     }
 
     // Get one professor
-    @GetMapping("/professors/{id}")
-    public ResponseEntity<Object> getOneProfessor(@PathVariable(value="id") UUID id) {
+    @GetMapping("/show/{id}")
+    public ModelAndView getOneProfessor(@PathVariable(value="id") UUID id) {
+        ModelAndView mv = new ModelAndView("professors/ReadOne");
         Optional<ProfessorModel> professorO = professorRepository.findById(id);
-        if(professorO.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Professor not found.");
-        }
-        professorO.get().add(linkTo(methodOn(ProfessorController.class).getAllProfessors()).withRel("Professors List"));
-        return ResponseEntity.status(HttpStatus.OK).body(professorO.get());
+        mv.addObject("name", professorO.get().getName());
+        mv.addObject("surname", professorO.get().getSurname());
+        mv.addObject("identification", professorO.get().getIdentification());
+        mv.addObject("email", professorO.get().getEmail());
+        mv.addObject("phone", professorO.get().getPhone());
+        mv.addObject("address", professorO.get().getAddress());
+        mv.addObject("city", professorO.get().getCity());
+        mv.addObject("state", professorO.get().getState());
+        mv.addObject("country", professorO.get().getCountry());
+        mv.addObject("zipCode", professorO.get().getZipCode());
+        mv.addObject("title", professorO.get().getTitle());
+        mv.addObject("department", professorO.get().getDepartment());
+        mv.addObject("siape", professorO.get().getSiape());
+        mv.addObject("course", professorO.get().getCourse());
+        return mv;
+    }
+
+    // Update a student form
+    @GetMapping("/update/{id}")
+    public ModelAndView updateProfessorGet(@PathVariable(value="id") UUID id) {
+        ModelAndView mv = new ModelAndView("professors/Update");
+        Optional<ProfessorModel> professorO = professorRepository.findById(id);
+        List<CourseModel> courses = courseRepository.findAll();
+        mv.addObject("name", professorO.get().getName());
+        mv.addObject("surname", professorO.get().getSurname());
+        mv.addObject("identification", professorO.get().getIdentification());
+        mv.addObject("email", professorO.get().getEmail());
+        mv.addObject("phone", professorO.get().getPhone());
+        mv.addObject("address", professorO.get().getAddress());
+        mv.addObject("city", professorO.get().getCity());
+        mv.addObject("state", professorO.get().getState());
+        mv.addObject("country", professorO.get().getCountry());
+        mv.addObject("zipCode", professorO.get().getZipCode());
+        mv.addObject("title", professorO.get().getTitle());
+        mv.addObject("department", professorO.get().getDepartment());
+        mv.addObject("siape", professorO.get().getSiape());
+        mv.addObject("courseAssigned", professorO.get().getCourse());
+        mv.addObject("courses", courses);
+        return mv;
     }
 
     // Update a professor
-    @PutMapping("/professors/{id}")
-    public ResponseEntity<Object>
-    updateProfessor(@PathVariable(value="id") UUID id, @RequestBody @Valid ProfessorRecordDto professorRecordDto) {
+    @PostMapping("/update/{id}")
+    public String updateStudentPost(@Valid @ModelAttribute ProfessorRecordDto professorRecordDto, BindingResult result, @PathVariable(value="id") UUID id) {
+
         Optional<ProfessorModel> professorO = professorRepository.findById(id);
         if(professorO.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Professor not found.");
+            return "redirect:/professors/";
+        }
+        if(result.hasErrors()) {
+            return "professors/Update";
         }
         ProfessorModel professorModel = professorO.get();
         BeanUtils.copyProperties(professorRecordDto, professorModel);
-        return ResponseEntity.status(HttpStatus.OK).body(professorRepository.save(professorModel));
+
+        CourseModel course = courseRepository.findById(professorRecordDto.courseId()).get();
+        professorModel.setCourse(course);
+
+        professorRepository.save(professorModel);
+        return "redirect:/professors/";
     }
 
     // Delete a professor
-    @DeleteMapping("/professors/{id}")
-    public ResponseEntity<Object> deleteProfessor(@PathVariable(value = "id") UUID id) {
+    @GetMapping("/delete/{id}")
+    public String deleteProfessor(@PathVariable(value="id") UUID id) {
         Optional<ProfessorModel> professorO = professorRepository.findById(id);
-        if (professorO.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Professor not found.");
+        if(professorO.isEmpty()) {
+            return "redirect:/professors/";
         }
         professorRepository.delete(professorO.get());
-        return ResponseEntity.status(HttpStatus.NO_CONTENT).body("Professor deleted.");
+        return "redirect:/professors/";
     }
-
-    // Set a course to a professor
-    @PutMapping("/professors/{idProfessor}/assign-course/{idCourse}")
-    public ResponseEntity<Object> assignCourseToProfessor(
-            @PathVariable UUID idProfessor,
-            @PathVariable UUID idCourse) {
-
-        Optional<ProfessorModel> professor0 = professorRepository.findById(idProfessor);
-        Optional<CourseModel> courseO = courseRepository.findById(idCourse);
-
-        if (professor0.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Professor not found.");
-        }
-        if(courseO.isEmpty()) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Course not found.");
-        }
-
-        ProfessorModel professor = professor0.get();
-        CourseModel course = courseO.get();
-
-        professor.setCourse(course);
-        professorRepository.save(professor);
-
-        return ResponseEntity.status(HttpStatus.OK).body("Course assigned to professor successfully.");
-    }
-
-
 
 }
--- src/main/java/com/takintosh/web2school/controllers/StudentController.java
+++ src/main/java/com/takintosh/web2school/controllers/StudentController.java
@@ -8,8 +8,6 @@ import com.takintosh.web2school.repositories.StudentRepository;
 import jakarta.validation.Valid;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
